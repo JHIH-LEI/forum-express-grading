ed55d039ffa9e1532869e75d38ea8baae7d0c82c
JHIH-LEI <vicky88888@gmail.com>
2021-08-23 20:48:05 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
+
+app.engine('handlebars', exphbs()) //handlebars註冊樣板engine
+app.set('view engine', 'handlebars') //設定使用handlebars作為樣板engine
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
