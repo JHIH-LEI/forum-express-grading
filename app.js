e42938b1b03afb464a2c2f0d65ec81fe095dd1ac
JHIH-LEI <vicky88888@gmail.com>
2021-08-24 01:55:12 +0800

    feat: install bcrypt and bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 3000
 const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //handlebars註冊樣板engine
 app.set('view engine', 'handlebars') //設定使用handlebars作為樣板engine
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
