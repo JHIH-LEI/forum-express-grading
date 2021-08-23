1a954a4026a471cb94e4eab43b4429e62a46091f
JHIH-LEI <vicky88888@gmail.com>
2021-08-24 03:58:42 +0800

    feat: install passport and passport-local

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('./config/passport')
 
 const port = 3000
 const db = require('./models')
@@ -17,6 +18,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -25,7 +28,7 @@ app.use((req, res, next) => {
   next()
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
