bb5527eeb53fd960435f3e7a83f121a21d841f01
JHIH-LEI <vicky88888@gmail.com>
2021-08-24 03:15:21 +0800

    feat: install express-session and connect-flash

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const port = 3000
 const db = require('./models')
 
@@ -9,11 +12,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //handlebars註冊�
 app.set('view engine', 'handlebars') //設定使用handlebars作為樣板engine
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+require('./routes')(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-require('./routes')(app)
 
 module.exports = app
