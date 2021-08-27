b2bad6583727077f76bdee44151446d0e6f03322
JHIH-LEI <vicky88888@gmail.com>
2021-08-27 19:24:43 +0800

    feat: Add a foolproof  to inform users the page is not yet open

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,8 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signinPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signin)
   app.get('/logout', userController.logout)
+  app.get('/users/:id', (req, res) => {
+    req.flash('error_messages', '用戶檔案功能還未實現，已將您返回主頁')
+    res.redirect('/restaurants')
+  })
 }
