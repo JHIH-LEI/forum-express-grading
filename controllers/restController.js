15bc9922d60005b61fd05f95ed8644c133fb7118
JHIH-LEI <vicky88888@gmail.com>
2021-09-02 03:53:25 +0800

    feat: add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,7 +1,20 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    try {
+      let restaurants = await Restaurant.findAll({ raw: true, nest: true, include: { model: Category, attributes: ['name'] } })
+      // 把要傳到前端的資料其餐廳描述做修改
+      restaurants = restaurants.map(rest => ({
+        ...rest, //其他資料不變
+        description: rest.description.substring(0, 50) //描述只要50字
+      }))
+      return res.render('restaurants', { restaurants })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
-
 module.exports = restController
