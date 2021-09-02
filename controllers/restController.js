78a31db3d6e2434784ffa87e287f8be6858eb720
JHIH-LEI <vicky88888@gmail.com>
2021-09-02 17:02:09 +0800

    feat: add pagination on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,25 +2,59 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: async (req, res) => {
     try {
-      // 如果queryString有類別，就要依據類別做篩選
+      let offset = 0
       const whereQuery = {}
       let categoryId = ''
+      // 抓到當前是第幾頁
+      let page = Number(req.query.page) || 1
+      // 計算偏移量，決定從第幾筆資料開始查找
+      if (req.query.page) {
+        offset = (page - 1) * pageLimit
+      }
+
+      // 如果queryString有類別，就要依據類別做篩選
       if (req.query.categoryId) {
         // 獲取篩選條件
         categoryId = Number(req.query.categoryId)
         whereQuery.CategoryId = categoryId // {CategoryId: 41}
       }
-      let restaurants = await Restaurant.findAll({ raw: true, nest: true, include: { model: Category, attributes: ['name'] }, where: whereQuery })
+
+      // 取得餐廳資料及餐廳總數
+      const result = await Restaurant.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: { model: Category, attributes: ['name'] },
+        where: whereQuery,
+        offset,
+        limit: pageLimit
+      })
       // 把要傳到前端的資料其餐廳描述做修改
-      restaurants = restaurants.map(rest => ({
+      const restaurants = result.rows.map(rest => ({
         ...rest, //其他資料不變
         description: rest.description.substring(0, 50) //描述只要50字
       }))
+
+      // 計算總頁數及前端所需的頁數陣列
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      // 計算上一頁
+      let prev = page - 1 || 1
+      // 計算下一頁
+      let next = page + 1 > pages ? page : page + 1
+      // 取得所有類別清單
       const categories = await Category.findAll({ raw: true, attributes: ['id', 'name'] })
-      return res.render('restaurants', { restaurants, categories, categoryId })
+
+      return res.render('restaurants', {
+        restaurants,
+        categories,
+        categoryId,
+        page, totalPage, prev, next
+      })
     } catch (err) {
       console.log(err)
     }
@@ -28,7 +62,7 @@ const restController = {
   getRestaurant: async (req, res) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, { include: { model: Category, attributes: ['name'] } })
-      res.render('restaurant', { restaurant: restaurant.toJSON() })
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
     } catch (err) {
       console.warn(err)
     }
