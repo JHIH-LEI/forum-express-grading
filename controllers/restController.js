e9a83cee8e73bd9f6864f06bb476ab1892dbbb24
JHIH-LEI <vicky88888@gmail.com>
2021-09-02 15:55:23 +0800

    feat: add getRestaurants controller category sort feature via queryString

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,13 +5,22 @@ const Category = db.Category
 const restController = {
   getRestaurants: async (req, res) => {
     try {
-      let restaurants = await Restaurant.findAll({ raw: true, nest: true, include: { model: Category, attributes: ['name'] } })
+      // 如果queryString有類別，就要依據類別做篩選
+      const whereQuery = {}
+      let categoryId = ''
+      if (req.query.categoryId) {
+        // 獲取篩選條件
+        categoryId = Number(req.query.categoryId)
+        whereQuery.CategoryId = categoryId // {CategoryId: 41}
+      }
+      let restaurants = await Restaurant.findAll({ raw: true, nest: true, include: { model: Category, attributes: ['name'] }, where: whereQuery })
       // 把要傳到前端的資料其餐廳描述做修改
       restaurants = restaurants.map(rest => ({
         ...rest, //其他資料不變
         description: rest.description.substring(0, 50) //描述只要50字
       }))
-      return res.render('restaurants', { restaurants })
+      const categories = await Category.findAll({ raw: true, attributes: ['id', 'name'] })
+      return res.render('restaurants', { restaurants, categories, categoryId })
     } catch (err) {
       console.log(err)
     }
