68f624997d5a4324cffb9734e08dbd7851e380d3
JHIH-LEI <vicky88888@gmail.com>
2021-09-05 02:40:21 +0800

    feat: modify user model self-referential part

--- models/user.js
+++ models/user.js
@@ -6,28 +6,33 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       User.hasMany(models.Comment)
+
       User.belongsToMany(models.Restaurant, {
         through: models.Favorite, //透過這個model找到關係
         foreignKey: 'UserId', //透過（固定）UserId去找
         as: 'FavoritedRestaurants' //model可透過這個方法找到關聯的資料,也就是找到後的資料要存到model資料底下的什麼變數
       })
+
       User.belongsToMany(models.Restaurant, {
         through: models.Like,
         foreignKey: 'UserId',
         as: 'LikedRestaurants'
       })
+
       // 一個使用者可以追蹤很多使用者（查他追蹤了哪些人）
-      User.belongsToMany(models.User, {
-        through: models.FollowShip,
-        foreignKey: 'followerId',
-        as: 'followings'
-      })
-      // 一個使用者可以被很多人追蹤，要查被哪些人追蹤，固定被追蹤的欄位
-      User.belongsToMany(models.User, {
-        through: models.FollowShip,
+      User.belongsToMany(User, {
+        through: models.Followship,
         foreignKey: 'followingId',
-        as: 'followers'
+        as: 'Followers'
       })
+
+      // 一個使用者可以被很多人追蹤，要查被哪些人追蹤，固定被追蹤的欄位
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+
     }
   };
   User.init({
