6867cf98bfac94f86c20b01626afdf2aa9a0f3eb
JHIH-LEI <vicky88888@gmail.com>
2021-09-08 03:36:07 +0800

    POST /api/signin (return jwt)

---
+++ controllers/api/userController.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+const userController = {
+  signIn: async (req, res) => {
+    try {
+      // 檢查必要資料
+      if (!req.body.email || !req.body.password) {
+        return res.json({ status: 'error', message: '所有欄位都是必填項！' })
+      }
+      // 檢查user是否存在與密碼是否正確
+      let username = req.body.email
+      let password = req.body.password
+
+      const user = await User.findOne({ where: { email: username } })
+      if (!user) {
+        return res.status(401).json({ status: 'error', message: 'no such user found' })
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ status: 'error', message: '帳號or密碼錯誤' })
+      }
+      // 簽發token
+      const payload = { id: user.id }
+      const token = jwt.sign(payload, process.env.JWT_SECRET)
+      return res.json({
+        status: 'success',
+        message: 'ok',
+        token,
+        user: { id: user.id, name: user.name, email: user.email, isAdmin: user.isAdmin }
+      })
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+}
+
+module.exports = userController
